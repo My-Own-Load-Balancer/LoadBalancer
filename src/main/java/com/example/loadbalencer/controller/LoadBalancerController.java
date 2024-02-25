ca99329bcdb60639acdcae80bed506652750971f
assia-jabri <assia.jabri@ump.ac.ma>
2024-02-25 15:16:53 +0100

    Balancing using round robin

--- src/main/java/com/example/loadbalencer/controller/LoadBalancerController.java
+++ src/main/java/com/example/loadbalencer/controller/LoadBalancerController.java
@@ -6,21 +6,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class LoadBalancerController {
 
     private final RestTemplate restTemplate;
-    private final String backendUrl;
+    private final List<String> backendUrls;
+    private int currentIndex;
 
     @Autowired
     public LoadBalancerController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-        this.backendUrl = "http://localhost:8081"; // Replace with your backend server URL
+        this.backendUrls = new ArrayList<>();
+        // Hardcoded backend server URLs for demonstration
+        backendUrls.add("http://localhost:8081");
+        backendUrls.add("http://localhost:8082");
+        this.currentIndex = 0;
+        System.out.println(backendUrls);
     }
 
     @GetMapping("/")
     public String forwardRequest() {
+        String backendUrl = getNextBackendUrl();
         ResponseEntity<String> response = restTemplate.getForEntity(backendUrl, String.class);
-        return "Response from server: " + response.getStatusCode() + "\n\n" + response.getBody();
+        return "Response from server " + backendUrl + ":\n" + response.getBody();
+    }
+
+    private synchronized String getNextBackendUrl() {
+        String nextBackendUrl = backendUrls.get(currentIndex);
+        currentIndex = (currentIndex + 1) % backendUrls.size();
+        return nextBackendUrl;
     }
 }
